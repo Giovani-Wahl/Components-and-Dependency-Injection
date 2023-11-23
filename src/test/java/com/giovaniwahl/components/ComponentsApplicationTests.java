e82af880c04f824b248ee776ce5623c14ea8d4f5
wahl <giovaniwahl@gmail.com>
2023-11-23 15:17:39 -0300

    inicio do projeto

---
+++ src/test/java/com/giovaniwahl/components/ComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovaniwahl.components;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
